bb6ede2f62c76cc7eceb77f4295b151254ce21e4
CarolinaArceEspinoza <carola.arce@gmail.com>
2024-01-29 14:57:12 -0500

    botones

--- js/app.js
+++ js/app.js
@@ -11,6 +11,43 @@ var storyButton6 = document.getElementById("completeStory");
 var storyButton7 = document.getElementById("reset");
 var storyButton8 = document.getElementById("randomStory");
 
+var turkey = document.getElementById("turkey");
+var mom = document.getElementById("mom");
+var dad = document.getElementById("dad");
+var dog = document.getElementById("dog");
+var teacher = document.getElementById("teacher");
+var elephant = document.getElementById("elephant");
+var cat = document.getElementById("cat");
+
+var sit = document.getElementById("sit");
+var eat = document.getElementById("eat");
+var dance = document.getElementById("dance");
+var saw = document.getElementById("saw");
+var dislike = document.getElementById("dislike");
+var kiss = document.getElementById("kiss");
+
+var funny = document.getElementById("funny");
+var scary = document.getElementById("scary");
+var goofy = document.getElementById("goofy");
+var slimy = document.getElementById("slimy");
+var bark = document.getElementById("bark");
+var fat = document.getElementById("fat");
+
+var goat = document.getElementById("goat");
+var monkey = document.getElementById("monkey");
+var fish = document.getElementById("fish");
+var cow = document.getElementById("cow");
+var frog = document.getElementById("frog");
+var bug = document.getElementById("bug");
+var worm = document.getElementById("worm");
+
+var moon = document.getElementById("moon");
+var chair = document.getElementById("chair");
+var spaguetti = document.getElementById("spaguetti");
+var soup = document.getElementById("soup");
+var grass = document.getElementById("grass");
+var shoes = document.getElementById("shoes");
+
 const nouns1 = new Array("The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat");
 const nouns2 = new Array("sat on", "ate", "danced with", "saw", "doesn't like", "kissed");
 const nouns3 = new Array("a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat");
@@ -36,6 +73,21 @@ var lecturatextos = function(phrases, linea, position, flag){
     }
     
   };
+
+
+  var lecturaNombre = function(phrases, position, linea){
+    var newquotes = phrases[position];
+    document.getElementById(linea).innerText = newquotes;
+    
+    completeText[position] = newquotes;
+synth.cancel();
+    
+    var textoCorrido = completeText.toString().replaceAll(",", " ");
+    speakNow(textoCorrido);
+
+};
+
+
 
 
 
@@ -84,6 +136,10 @@ function randomStory() {
     
 }
 
+
+
+
+
 /* Event Listeners
 -------------------------------------------------- */
 // Onclick handler for the button that speaks the text contained in the above var textToSpeak
@@ -121,3 +177,106 @@ storyButton7.onclick = function() {
 storyButton8.onclick = function() {
     randomStory();
 }
+turkey.onclick = function() {
+    lecturaNombre(nouns1, 0, "listBox1");
+}
+mom.onclick = function() {
+    lecturaNombre(nouns1, 1, "listBox1");
+}
+dad.onclick = function() {
+    lecturaNombre(nouns1, 2, "listBox1");
+}
+dog.onclick = function() {
+    lecturaNombre(nouns1, 3, "listBox1");
+}
+teacher.onclick = function() {
+    lecturaNombre(nouns1, 4, "listBox1");
+}
+elephant.onclick = function() {
+    lecturaNombre(nouns1, 5, "listBox1");
+}
+cat.onclick = function() {
+    lecturaNombre(nouns1, 6, "listBox1");
+}
+
+sit.onclick = function() {
+    lecturaNombre(nouns2, 0, "listBox2");
+}
+eat.onclick = function() {
+    lecturaNombre(nouns2, 1, "listBox2");
+}
+dance.onclick = function() {
+    lecturaNombre(nouns2, 2, "listBox2");
+}
+saw.onclick = function() {
+    lecturaNombre(nouns2, 3, "listBox2");
+}
+dislike.onclick = function() {
+    lecturaNombre(nouns2, 4, "listBox2");
+}
+kiss.onclick = function() {
+    lecturaNombre(nouns2, 5, "listBox2");
+}
+
+funny.onclick = function() {
+    lecturaNombre(nouns3, 0, "listBox3");
+}
+scary.onclick = function() {
+    lecturaNombre(nouns3, 1, "listBox3");
+}
+goofy.onclick = function() {
+    lecturaNombre(nouns3, 2, "listBox3");
+}
+slimy.onclick = function() {
+    lecturaNombre(nouns3, 3, "listBox3");
+}
+bark.onclick = function() {
+    lecturaNombre(nouns3, 4, "listBox3");
+}
+fat.onclick = function() {
+    lecturaNombre(nouns3, 5, "listBox3");
+}
+
+goat.onclick = function() {
+    lecturaNombre(nouns4, 0, "listBox4");
+}
+monkey.onclick = function() {
+    lecturaNombre(nouns4, 1, "listBox4");
+}
+fish.onclick = function() {
+    lecturaNombre(nouns4, 2, "listBox4");
+}
+cow.onclick = function() {
+    lecturaNombre(nouns4, 3, "listBox4");
+}
+frog.onclick = function() {
+    lecturaNombre(nouns4, 4, "listBox4");
+}
+bug.onclick = function() {
+    lecturaNombre(nouns4, 5, "listBox4");
+}
+worm.onclick = function() {
+    lecturaNombre(nouns4, 6, "listBox4");
+}
+
+moon.onclick = function() {
+    lecturaNombre(nouns5, 0, "listBox5");
+}
+chair.onclick = function() {
+    lecturaNombre(nouns5, 1, "listBox5");
+}
+spaguetti.onclick = function() {
+    lecturaNombre(nouns5, 2, "listBox5");
+}
+soup.onclick = function() {
+    lecturaNombre(nouns5, 3, "listBox5");
+}
+grass.onclick = function() {
+    lecturaNombre(nouns5, 4, "listBox5");
+}
+shoes.onclick = function() {
+    lecturaNombre(nouns5, 5, "listBox5");
+}
+
+
+
